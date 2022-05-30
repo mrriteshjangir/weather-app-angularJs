df12437896bdb9a2140d7160b4ebfc7df639ea81
mrriteshjangir <mrriteshjangir@gmail.com>
2022-05-30 18:13:55 +0530

    return & array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather-app-angularJs';
+
+  public text: string = "Hello";
+  public caption: string = "Click Me!"; 
+
+  places: string[] = ['bhandara','nagpur','gondia','pune','mumbai','goa'];
+
+  private count:number = 0; 
+
+  ngOnInit(): void {
+  }
+
+  change():any{
+    this.caption = "Clicked!";
+  }
+
+  getCurrentTime(): any {
+    return new Date(Date.now()).toLocaleTimeString("en-US");
+  }
+
+  getCurrentDate():any{
+    return new Date(Date.now()).toLocaleDateString("en-US");
+  }
+
+  num = 0;
 }
